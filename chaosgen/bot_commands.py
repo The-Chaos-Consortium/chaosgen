c85f1244ce5542edfa8a9d49f740c4882f9dc0b2
Alexander Gomez <alex@gomezgang.com>
2023-05-01 03:59:58 -0400

    feat: finish random chargen for bot

--- chaosgen/bot_commands.py
+++ chaosgen/bot_commands.py
@@ -20,4 +20,24 @@ def random_character():
         f"**Slots**: {char.slots}\n" +
         f"**Equipment**: \n{newline.join(f'{index+1}. {equip}' for index, equip in enumerate(char.equipment))}\n"
     )
+    if char.mount:
+        data += (
+            f"\n**Mount Type**: {char.mount['name']}\n" +
+            f"**Mount HP**: {dice.xdy(char.mount['HD'], 8)}\n" +
+            f"**Mount Skill**: {char.mount['Skill']}\n" +
+            f"**Mount Morale**: {char.mount['ML']}\n" +
+            f"**Mount Slots**: {char.mount['Slots']}\n"
+        )
+        if char.mount["equipment"]:
+            data += f"**Mount Equipment**: \n{newline.join(f'{index+1}. {equip}' for index, equip in enumerate(char.mount['equipment']))}\n"
+    if char.retainer:
+        data += (
+            f"\n**Retainer Type**: {char.retainer['name']}\n" +
+            f"**Retainer HP**: {dice.xdy(char.retainer['HD'], 8)}\n" +
+            f"**Retainer Skill**: {char.retainer['Skill']}\n" +
+            f"**Retainer Morale**: {char.retainer['ML']}\n" +
+            f"**Retainer Level**: 1\n"
+            f"**Retainer Slots**: 10\n"
+            f"**Retainer Equipment**: \n{newline.join(f'{index+1}. {equip}' for index, equip in enumerate(char.retainer['equipment']))}\n"
+        )
     return data
